c14d0ea79ee4af17eb024204a2c056fb250527a6
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-05-21 16:39:43 +0800

    update

---
+++ LS3/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace LS3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 存储空间
+            int sbyteSize = sizeof(sbyte);
+            Console.WriteLine($"sbyte占用{sbyteSize}个字节");
+
+            int shortSize = sizeof(short);
+            Console.WriteLine($"short占用{shortSize}个字节");
+
+            int intSize = sizeof(int);
+            Console.WriteLine($"int占用{intSize}个字节");
+
+            int longSize = sizeof(long);
+            Console.WriteLine($"long占用{longSize}个字节");
+
+            Console.WriteLine("------------------------");
+
+            int byteSize = sizeof(byte);
+            Console.WriteLine($"byte占用{byteSize}个字节");
+
+            int ushortSize = sizeof(ushort);
+            Console.WriteLine($"ushort占用{ushortSize}个字节");
+
+            int uintSize = sizeof(uint);
+            Console.WriteLine($"uint占用{uintSize}个字节");
+
+            int ulongSize = sizeof(ulong);
+            Console.WriteLine($"ulong占用{ulongSize}个字节");
+
+            Console.WriteLine("------------------------");
+
+            int floatSize = sizeof(float);
+            Console.WriteLine($"float占用{floatSize}个字节");
+
+            int doubleSize = sizeof(double);
+            Console.WriteLine($"double占用{doubleSize}个字节");
+
+            int decimalSize = sizeof(decimal);
+            Console.WriteLine($"decimal占用{decimalSize}个字节");
+
+            Console.WriteLine("------------------------");
+            
+            int charSize = sizeof(char);
+            Console.WriteLine($"char占用{charSize}个字节");
+
+            int boolSize = sizeof(bool);
+            Console.WriteLine($"bool占用{boolSize}个字节");
+
+            // string没有固定的存储空间
+
+            #endregion
+
+            #region 变量的本质
+            // 二进制
+            // 1字节 = 8位（byte）
+
+
+            #endregion
+        }
+    }
+}
---
+++ LS4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LS4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 命名规范必须遵守的规则
+            // 1.不能重名
+            // 2.不能以数字开头，但可以包含数字，也可以以下划线开头
+            // 3.不能使用关键字，如int,string,if...
+            // 4.不能使用特殊字符，但可以使用下划线
+            #endregion
+
+            #region 常用命名规范
+            // 1.帕斯卡命名法（大驼峰）：每个单词的首字母大写，如：MyName；常用于类、方法名
+            // 2.小驼峰命名法（小驼峰）：第一个单词首字母小写，其余单词首字母大写，如：myName；常用于变量名
+            #endregion
+        }
+    }
+}
