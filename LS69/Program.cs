079e079a2ddd245efcc6fb6f8a03585c6c6b5364
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-06-07 21:17:33 +0800

    update

---
+++ LS69/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+
+namespace LS69
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ArrayList
+            // ArrayList本质是object类型的数组
+            // C#已经封装好了，有增删查改的方法
+
+            // 声明
+            // 要引用命名空间
+            ArrayList list = new ArrayList();
+
+            // 增
+            list.Add(1);
+            list.Add("hello");
+            list.Add(1.1);
+            list.Insert(0, "world"); // 在第一个位置插入
+
+            ArrayList list2 = new ArrayList();
+            list2.Add(2);
+            list.AddRange(list2);
+
+            // 删
+            list.Remove(1); // 删除第一个1
+            list.RemoveAt(0); // 删除第一个元素
+            list2.Clear(); // 清空
+
+            // 查
+            Console.WriteLine(list[0]);
+
+            if (list.Contains(1.1))
+            {
+                Console.WriteLine("包含");
+            }
+
+            // 正向查找，返回索引
+            // 找不到返回-1
+            int index = list.IndexOf("123");
+            Console.WriteLine(index);
+
+            // 反向查找
+            index = list.LastIndexOf("123");
+            Console.WriteLine(index);
+
+            // 改
+            list[0] = "123";
+            Console.WriteLine(list[0]);
+
+            // 遍历
+            Console.WriteLine(list.Count); // 数组长度
+            Console.WriteLine(list.Capacity); // 容量
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            // 装箱拆箱
+            // 因为AL是object类型的数组，它在进行数据存储和读取中肯定存在装箱拆箱
+            #endregion
+        }
+    }
+}
---
+++ LS70/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+namespace LS70
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Stack
+            // Stack是一个object类型的数组，但封装了特殊的存储规则
+            // 是栈存储容器，先进后出
+
+            // 声明
+            Stack stack = new Stack();
+
+            // 增（压栈）
+            stack.Push(1);
+            stack.Push("123");
+            stack.Push(1.1);
+            stack.Push(true);
+            stack.Push('a');
+
+            // 取
+            object v = stack.Pop();
+            Console.WriteLine(v);
+
+            v = stack.Pop();
+            Console.WriteLine(v);
+
+            // 查
+            // 栈无法查看指定位置的内容，只能查看栈顶的内容
+            Console.WriteLine(stack.Peek()); // Peek不弹出，只查看
+            // 查看元素是否存在
+            if (stack.Contains(1))
+            {
+                Console.WriteLine("存在");
+            }
+
+            // 栈没有办法改变其中元素，要么清空再次压栈、弹出
+
+            // 遍历
+            Console.WriteLine(stack.Count); // 长度
+            // 栈不能用for遍历，只能用foreach
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            // 可以将栈转为object数组，再用for遍历
+            object[] arr = stack.ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+            // 循环弹栈
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+            #endregion
+        }
+    }
+}
